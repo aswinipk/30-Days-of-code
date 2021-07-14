2271bfbd4100cbcc46036afd6d4025973448d806
aswinipk <aswinikannan96@gmail.com>
2021-07-14 14:44:01 +0530

    code for day 1

---
+++ Day1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Types
+# 
+# 
+
+# Complete the code in the editor below. The variables i,d , and s  are already declared and initialized for you. You must:
+# 
+# Declare 3 variables: one of type int, one of type double, and one of type String.
+# Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your 3 variables.
+# Use the + operator to perform the following operations:
+# Print the sum of i plus your int variable on a new line.
+# Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+# Concatenate s with the string you read as input and print the result on a new line.
+
+# In[ ]:
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+k=int(input())
+l=float(input())
+m=input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+sum1=i+k
+print(sum1)
+
+# Print the sum of the double variables on a new line.
+sum2=d+l
+print(sum2)
+# Concatenate and print the String variables on a new line
+print(s+""+m)
+# The 's' variable above should be printed first.
+
