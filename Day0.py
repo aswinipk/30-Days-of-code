a5029ea5e907f0299496f1c2564965b73a6cc84a
aswinipk <aswinikannan96@gmail.com>
2021-07-14 14:53:54 +0530

    day0 work

---
+++ Day0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Hello, World
+
+# you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
+
+# In[ ]:
+
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
