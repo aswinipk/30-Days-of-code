ee8a0d1767f856a66e51f216e18dbf602d12426f
aswinipk <aswinikannan96@gmail.com>
2021-07-16 15:54:50 +0530

    code for day 2

---
+++ day2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Operators
+# 
+# 
+
+# Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+
+# In[ ]:
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    tip_percent=(tip_percent/100)*meal_cost
+    tax_percent=(tax_percent/100)*meal_cost
+    meal_cost=meal_cost+tip_percent+tax_percent
+    print(round(meal_cost))
+        
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
+
